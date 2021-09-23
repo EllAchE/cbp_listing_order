317fe946e3dd0afd0e10377a06c9840db1a5a59f
EllAchE <lhar8771@gmail.com>
2021-09-22 23:00:28 -0500

    adding packages

--- src/check_rss.ts
+++ src/check_rss.ts
@@ -1,12 +1,14 @@
 const got = require('got');
+const crypto1 = require('crypto');
 
 const cronString = `0 * 23,7-23 * * *`;
 // run every minute, all hourse except midnight-7am. Need to check TZ
 // also could probably ignore saturdays as possible listing date
 
 // run for both cbp and cb
-got(site.url).then(response => {
+got('https://blog.coinbase.com/feed').then(response => {
     const parser = new rss();
-    content = await parser.parseString(response.body)
-    content = content.items[0][site.contentSelector];
+    const content = await parser.parseString(response.body)
+    const firstItem = content.items[0]['title']; // other option is content:encoded
+    const hash = crypto.createHash(`md5`).update(content).digest(`hex`); //Get content of site
 } break;
