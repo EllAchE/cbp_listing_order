1453a899ee8ef6d0793cefb8ee66e31378420bf4
EllAchE <lhar8771@gmail.com>
2021-10-28 00:55:40 -0500

    adding logging for startup of cronjob

--- dist/check_rss_runner.js
+++ dist/check_rss_runner.js
@@ -1,5 +1,7 @@
 "use strict";
 exports.__esModule = true;
 var check_rss_1 = require("./check_rss");
+var logger_1 = require("./logger");
+logger_1.logger.info('Launching coinbase monitoring cronjob', new Date().toLocaleDateString());
 check_rss_1.cronUpdate.start(); // launch the cron
 //# sourceMappingURL=check_rss_runner.js.map
--- src/check_rss_runner.ts
+++ src/check_rss_runner.ts
@@ -1,3 +1,5 @@
 import { cronUpdate } from "./check_rss";
+import { logger } from "./logger";
 
+logger.info('Launching coinbase monitoring cronjob', new Date().toLocaleDateString())
 cronUpdate.start() // launch the cron
